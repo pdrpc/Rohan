180af5e0062860e70a643152c142526d48773b7f
pdrpc <pdr.pradocamargo@gmail.com>
2020-11-13 23:14:16 -0300

    Minor changes, Interface de login

--- app/src/main/java/br/usjt/rohan/DashboardActivity.java
+++ app/src/main/java/br/usjt/rohan/DashboardActivity.java
@@ -8,7 +8,6 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -19,7 +18,7 @@ public class DashboardActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
-    private static final int GPS_REUEST_PERMISSION_CODE = 1001;
+    private static final int GPS_REQUEST_PERMISSION_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +43,13 @@ public class DashboardActivity extends AppCompatActivity {
             }else{ActivityCompat.requestPermissions(
                         this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        GPS_REUEST_PERMISSION_CODE);
+                    GPS_REQUEST_PERMISSION_CODE);
             }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == GPS_REUEST_PERMISSION_CODE) {
+        if (requestCode == GPS_REQUEST_PERMISSION_CODE) {
             if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 locationManager.requestLocationUpdates(
