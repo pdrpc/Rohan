1311e068957abcd77e0f1e0f520e48df028927c2
RLemonache <49251607+RLemonache@users.noreply.github.com>
2020-11-14 19:34:25 -0300

    Inteface novo local

--- app/src/main/java/br/usjt/rohan/NewLocationActivity.java
+++ app/src/main/java/br/usjt/rohan/NewLocationActivity.java
@@ -1,14 +1,26 @@
 package br.usjt.rohan;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewLocationActivity extends AppCompatActivity {
+
+    private EditText editTextLocationName;
+    private EditText editTextLocationDesc;
+    private Button saveLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_location);
+    }
+
+    public void saveLocation(View view) {
+        String name = editTextLocationName.getEditableText().toString();
+        String desc = editTextLocationDesc.getEditableText().toString();
+
     }
 }
