0280898320ffbd7eacf2bd2f515ff1ebe30c162c
pdrpc <pdr.pradocamargo@gmail.com>
2020-11-17 06:42:01 -0300

    changed locationManager to fusedAPI Working realtime updates on coord.
    TODO:
    review lifecycles.
     moleMode.
     tests(done but it can always be more thoroughly tested)
     final interface(Initiated).

--- app/src/main/java/br/usjt/rohan/ui/DashboardActivity.java
+++ app/src/main/java/br/usjt/rohan/ui/DashboardActivity.java
@@ -40,9 +40,7 @@ import br.usjt.rohan.model.Location;
 
 public class DashboardActivity extends AppCompatActivity implements FirestoreAdapter.OnListItemLongClick {
 
-    private static final String TAG = "DashboardActivity";
-    private LocationManager locationManager;
-    private LocationListener locationListener;
+
     private static final int GPS_REQUEST_PERMISSION_CODE = 1001;
     private RecyclerView locationList;
     private FirebaseFirestore firebaseFirestore;
@@ -56,10 +54,6 @@ public class DashboardActivity extends AppCompatActivity implements FirestoreAda
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationListener = location -> {
-
-        };
         locationList = findViewById(R.id.firestore_list);
         firebaseFirestore = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
@@ -68,16 +62,16 @@ public class DashboardActivity extends AppCompatActivity implements FirestoreAda
 
 
         //Query
-            Query query = firebaseFirestore.collection(collection);
+        Query query = firebaseFirestore.collection(collection);
         //RecyclerView Opções
-            FirestoreRecyclerOptions<Location> options = new FirestoreRecyclerOptions.Builder<Location>()
-                    .setQuery(query, Location.class)
-                    .build();
-            adapter = new FirestoreAdapter(options, this);
+        FirestoreRecyclerOptions<Location> options = new FirestoreRecyclerOptions.Builder<Location>()
+                .setQuery(query, Location.class)
+                .build();
+        adapter = new FirestoreAdapter(options, this);
 
-            locationList.setHasFixedSize(true);
-            locationList.setLayoutManager(new LinearLayoutManager(this));
-            locationList.setAdapter(adapter);
+        locationList.setHasFixedSize(true);
+        locationList.setLayoutManager(new LinearLayoutManager(this));
+        locationList.setAdapter(adapter);
     }
 
 
@@ -86,35 +80,6 @@ public class DashboardActivity extends AppCompatActivity implements FirestoreAda
     protected void onStart(){
         super.onStart();
         adapter.startListening();
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                locationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER,
-                        5000,
-                        10,
-                        locationListener
-                );
-            }else{ActivityCompat.requestPermissions(
-                        this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    GPS_REQUEST_PERMISSION_CODE);
-            }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == GPS_REQUEST_PERMISSION_CODE) {
-            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER,
-                        5000,
-                        10,
-                        locationListener);
-                }
-            }else{
-                Toast.makeText(this, "Sem permissões", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 
     public void newLocation(View view){
@@ -125,6 +90,7 @@ public class DashboardActivity extends AppCompatActivity implements FirestoreAda
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+
     }
 
     @Override
--- app/src/main/java/br/usjt/rohan/ui/NewLocationActivity.java
+++ app/src/main/java/br/usjt/rohan/ui/NewLocationActivity.java
@@ -1,12 +1,16 @@
 package br.usjt.rohan.ui;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -14,10 +18,25 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CancellationSignal;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -31,8 +50,9 @@ import java.util.function.Consumer;
 import br.usjt.rohan.R;
 
 public class NewLocationActivity extends AppCompatActivity {
-    private static final String TAG = "NewLocationActivity";
 
+    private static final String TAG = "NewLocationActivity";
+    int LOCATION_REQUEST_CODE = 1001;
     private static final String KEY_NAME = "location_name";
     private static final String KEY_DESCRIPTION = "description";
     private static final String KEY_COORDINATES = "coordinates";
@@ -40,7 +60,31 @@ public class NewLocationActivity extends AppCompatActivity {
 
     private EditText editTextLocationName;
     private EditText editTextLocationDesc;
+    private TextView textViewLocationLat;
+    private TextView textViewLocationLon;
     private String collection;
+
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationCallback locationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            if (locationResult == null) {
+                return;
+            } else {
+                for (Location location : locationResult.getLocations()) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+                textViewLocationLat.setText("Lat : " + latitude);
+                textViewLocationLon.setText("Lon : " + longitude);
+            }
+        }
+    };
+
+    LocationRequest locationRequest;
 
     public double latitude;
     public double longitude;
@@ -49,14 +93,6 @@ public class NewLocationActivity extends AppCompatActivity {
     GregorianCalendar gc = new GregorianCalendar();
     String dataCriada = sdf.format(gc.getTime());
 
-    private LocationManager locationManager;
-    private CancellationSignal cancellationSignal;
-    private Executor executor;
-    private Location location;
-    public Consumer consumer;
-
-    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-    private FirebaseAuth auth = FirebaseAuth.getInstance();
 
 
     @RequiresApi(api = Build.VERSION_CODES.R)
@@ -65,30 +101,92 @@ public class NewLocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_location);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         collection = auth.getUid();
 
         editTextLocationName = findViewById(R.id.editTextLocationName);
         editTextLocationDesc = findViewById(R.id.editTextLocationDesc);
+        textViewLocationLat = findViewById(R.id.textViewLocationLat);
+        textViewLocationLon = findViewById(R.id.textViewLocationLon);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(2000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            checkSettingAndStarUpdates();
+        } else {
+            askLocationPermission();
         }
+    }
 
-//        Location location = locationManager.getCurrentLocation(LocationManager.GPS_PROVIDER, cancellationSignal, executor, Consumer<this.location> consumer);
+    private void checkSettingAndStarUpdates(){
+        LocationSettingsRequest request = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+        SettingsClient client = LocationServices.getSettingsClient(this);
 
-        Location location = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        Task<LocationSettingsResponse> locationSettingsResponseTask = client.checkLocationSettings(request);
+        locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                startLocationUpdates();
+            }
+        });
+        locationSettingsResponseTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException){
+                    ResolvableApiException apiException = (ResolvableApiException) e;
+                    try {
+                        apiException.startResolutionForResult(NewLocationActivity.this, 1001);
+                    } catch (IntentSender.SendIntentException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
 
-        onLocationChanged(location);
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates(){
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
 
     }
 
-    public void onLocationChanged (Location location){
-        this.longitude = location.getLongitude();
-        this.latitude = location.getLatitude();
+    private void stopLocationUpdates(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
+
+    private void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Log.d(TAG, "askLocationPemission: You should show an alert dialog...");
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                checkSettingAndStarUpdates();
+            } else {
+
+            }
+        }
+    }
+
+
 
 
     public void saveLocation(View view) {
@@ -112,4 +210,23 @@ public class NewLocationActivity extends AppCompatActivity {
 
         startActivity(new Intent(this, DashboardActivity.class));
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
+
 }
